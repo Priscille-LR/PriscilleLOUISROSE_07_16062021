47f85ff251bc5aa3906c67df6caac415b64d3e6a
Priscille-LR <76170675+Priscille-LR@users.noreply.github.com>
2021-06-23 18:02:57 +0200

    dropdowns refactoring

--- src/scripts/Dropdown.ts
+++ src/scripts/Dropdown.ts
@@ -3,7 +3,7 @@ import { recipesList } from './recipesList';
 
 export class Dropdown {
   ingredientsMap = new Map();
-  applianceMap = new Map();
+  appliancesMap = new Map();
   utensilsMap = new Map();
 
   createDropdowns(recipesList: Array<Recipe>) {
@@ -14,83 +14,51 @@ export class Dropdown {
     recipesList.forEach((recipe) => {
       const id = recipe.id;
       recipe.ingredients.forEach((ingredient) => {
-        let ingredientEl = ingredient.ingredient.toLowerCase();
-        ingredientEl =
-          ingredientEl.charAt(0).toUpperCase() + ingredientEl.slice(1);
-
-        if (this.ingredientsMap.has(ingredientEl)) {
-          let arrayIngredients = this.ingredientsMap.get(ingredientEl); 
-          arrayIngredients.push(id); 
-          this.ingredientsMap.set(ingredientEl, arrayIngredients); 
-        } else {
-          this.ingredientsMap.set(ingredientEl, [id]);
-        }
+        this.createMap(ingredient.ingredient, recipe.id, this.ingredientsMap);
       });
     });
-
-    let ingredientsKeys = Array.from(this.ingredientsMap.keys());
-
-    let ingredientsList = ingredientsKeys.map((ingredient) => {
-      return `<li class="ingredient">${ingredient}</li>`;
-    });
-
-    ingredientsList.forEach((liIngredient) => {
-      dropdownIngredients.innerHTML =
-        dropdownIngredients.innerHTML + liIngredient;
-    });
-
-
+    this.createLi(dropdownIngredients, this.ingredientsMap, "ingredient");
 
 
     recipesList.forEach((recipe) => {
-      const id = recipe.id;
-      let appliance = recipe.appliance.toLowerCase();
-      appliance = appliance.charAt(0).toUpperCase() + appliance.slice(1);
-      console.log(appliance);
-
-      if (this.applianceMap.has(appliance)) {
-        let arrayAppliances = this.applianceMap.get(appliance);
-        arrayAppliances.push(id);
-        this.applianceMap.set(appliance, arrayAppliances);
-      } else {
-        this.applianceMap.set(appliance, [id]);
-      }
-    });
-
-    let appliancesKeys = Array.from(this.applianceMap.keys());
-    let appliancesList = appliancesKeys.map((appliance) => {
-      return `<li class="ingredient">${appliance}</li>`;
-    });
-    appliancesList.forEach((liAppliance) => {
-      dropdownAppliances.innerHTML = dropdownAppliances.innerHTML + liAppliance;
-    });
-
-
-
+      this.createMap(recipe.appliance, recipe.id, this.appliancesMap);
+    })
+    this.createLi(dropdownAppliances, this.appliancesMap, "appliance")
 
 
     recipesList.forEach((recipe) => {
       const id = recipe.id;
       recipe.utensils.forEach((utensil) => {
-        let utensilEL = utensil.toLowerCase();
-        utensilEL = utensilEL.charAt(0).toUpperCase() + utensilEL.slice(1);
-
-        if (this.utensilsMap.has(utensilEL)) {
-          let arrayUtensils = this.utensilsMap.get(utensilEL);
-          arrayUtensils.push(id);
-          this.utensilsMap.set(utensilEL, arrayUtensils);
-        } else {
-          this.utensilsMap.set(utensilEL, [id]);
-        }
+        this.createMap(utensil, id, this.utensilsMap);
       });
     });
+    this.createLi(dropdownUtensils, this.utensilsMap, "utensil")
+  }
 
-    let utensilsKeys = Array.from(this.utensilsMap.keys());
-    let utensilsList = utensilsKeys.map((utensil) => {
-      return `<li class="ingredient">${utensil}</li>`;
+
+  private createLi(HTMLElement: Element, map: Map<string, Array<number>>, className: string) {
+    let filterKeys = Array.from(map.keys());
+
+    let filterList = filterKeys.map((element) => {
+      return  `<li class="${className}">${element}</li>`;
     });
-    utensilsList.forEach((liUtensil) => {
-      dropdownUtensils.innerHTML = dropdownUtensils.innerHTML + liUtensil;
+
+    filterList.forEach((li) => {
+      HTMLElement.innerHTML = HTMLElement.innerHTML + li;
     });
+  }
+
+
+  private createMap(filter: string, id: number, map: Map<String, Array<Number>>) {
+    let key = filter.toLowerCase();
+    key = key.charAt(0).toUpperCase() + key.slice(1);
+
+    if (map.has(key)) {
+      let array = map.get(key);
+      array.push(id);
+      map.set(key, array);
+    } else {
+      map.set(key, [id]);
+    }
   }
 }
